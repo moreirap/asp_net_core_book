2dc714103de653103e3e0dbd81b66a76ef69fb2a
Pedro Moreira <pedro.miguel.moreira@gmail.com>
2017-09-26 13:24:57 +0100

    Chapter 1 files

---
+++ Startup.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace hwapp
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory log)
+        {
+            app.Run(async (context) => 
+            {
+                await context.Response.WriteAsync("Hello world!");
+            });
+        }
+    }
+}
